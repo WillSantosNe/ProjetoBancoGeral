4d42fb13b12422ddb9478ec58ab9c7277eed3f58
William Dalla Stella dos Santos <williamsantosstella@gmail.com>
2023-11-24 17:35:39 -0300

    Ajuste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,20 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
+import { CartaosService } from './cartaos.service';
+import { CartaosComponent } from './components/cartaos/cartaos.component';
+
+import { ClientesService } from './clientes.service';
+import { ClientesComponent } from './components/clientes/clientes.component';
+
 import { ContasService } from './contas.service';
 import { ContasComponent } from './components/contas/contas.component';
 
 import { SeguroService } from './seguros.service';
 import { SegurosComponent } from './components/seguros/seguros.component';
+
+import { TransferenciasService } from './transferencias.service';
+import { TransferenciasComponent } from './components/transferencias/transferencias.component';
 
 @NgModule({
   declarations: [
@@ -20,9 +29,17 @@ import { SegurosComponent } from './components/seguros/seguros.component';
   ],
   providers: [
     HttpClientModule,
+    CartaosService,
+    ClientesService,
     ContasService,
-    SeguroService
+    EmprestimosService,
+    InvestimentosService,
+    SeguroService,
+    TransferenciasService
   ]
 })
+
 export class AppModule { }
+import { EmprestimosService } from './emprestimos.service';
+import { InvestimentosService } from './investimentos.service';
 
