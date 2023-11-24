9b9ce6e747577d3f428b35e677bdbf82961c9ebd
Mateus Bassi <mateus.s.bassi@gmail.com>
2023-11-24 18:04:05 -0300

    Adicionando_angular

--- src/app/transferencias.service.ts
+++ src/app/transferencias.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { ITransferencia } from './Transferencia';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class TransferenciasService {
 
-  constructor() { }
+export class EmprestimosService {
+  baseUrl = 'http://localhost:5000/transferencia'; //Rota padrao
+  constructor(private http: HttpClient) { } 
+
+  newRoom(ITransferencia: ITransferencia): Observable<any> {
+    const url = `${this.baseUrl}/add-transferencia`;
+    return this.http.post<ITransferencia>(url, ITransferencia, httpOptions);
+  }
 }
