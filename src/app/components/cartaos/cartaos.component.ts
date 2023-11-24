d66ea898b4be5da0b32f6c1fe4d60af37c22d4b1
ViniciusSito <itoviniciusshigueyuki@gmail.com>
2023-11-24 18:18:12 -0300

    Atualizando projeto

--- src/app/components/cartaos/cartaos.component.ts
+++ src/app/components/cartaos/cartaos.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CartaoService } from '../../cartao.service';
+import { Cartao } from '../../cartao';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-cartaos',
+  selector: 'app-cartoes',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './cartaos.component.html',
-  styleUrl: './cartaos.component.css'
+  templateUrl: './cartoes.component.html',
+  styleUrls: ['./cartoes.component.css']
 })
-export class CartaosComponent {
+export class CartoesComponent implements OnInit {
+  formulario: any;
+  titulo: string = '';
 
+  constructor(private cartaoService: CartaoService) { }
+
+  ngOnInit(): void {
+    this.titulo = 'Novo Cartão';
+    this.formulario = new FormGroup({
+      id: new FormControl(null),
+      numcartao: new FormControl(null),
+      dataval: new FormControl(null),
+      codigoseg: new FormControl(null),
+      limite: new FormControl(null),
+      bloqueado: new FormControl(null),
+
+      contaId: new FormControl(null)
+    });
+  }
+
+  enviarFormulario(): void {
+    const cartao: Cartao = this.formulario.value;
+    this.cartaoService.criar(cartao).subscribe(result => {
+      alert("Cartão inserido");
+    });
+  }
 }
--- src/app/components/investimentos/investimentos.component.ts
+++ src/app/components/investimentos/investimentos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Investimento } from '../../Investimento';
 
 @Component({
   selector: 'app-investimentos',
@@ -8,6 +9,30 @@ import { CommonModule } from '@angular/common';
   templateUrl: './investimentos.component.html',
   styleUrl: './investimentos.component.css'
 })
-export class InvestimentosComponent {
+export class InvestimentosComponent implements OnInit{
+  formulario : any;
+  titulo : string = '';
+
+  constructor(private investimentoService: InvestimentosService){ }
+
+  ngOnInit(): void{
+    this.titulo = 'Novo Investimento';
+    this.formulario = new FormGroup({
+     id : new FormControl(null)
+     valorini : new FormControl(null)
+     datainvest : new FormControl(null)
+     rentabilidade : new FormControl(null)
+     dataresgate : new FormControl(null)
+     taxa : new FormControl(null)
+
+     tipoinvestimento : new FormControl(null)
+
+     contaId : new FormControl(null)
+    })
+  }
+  enviarFormulario():void{
+    const investimento : Investimento = this.formulario.value;
+    this.investimentoService.criar(investimento).subscribe(result =>{alert("Investimento inserido")})
+  }
 
 }
