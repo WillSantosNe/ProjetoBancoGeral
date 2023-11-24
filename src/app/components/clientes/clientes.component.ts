875cc7f1dd60009b3102ee78f6afd4de72cf3ef6
Mateus Bassi <mateus.s.bassi@gmail.com>
2023-11-24 18:23:31 -0300

    ajustes

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-clientes',
@@ -8,6 +9,26 @@ import { CommonModule } from '@angular/common';
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
-export class ClientesComponent {
+export class ClientesComponent implements OnInit {
+  formulario : any;
+  tituloFormulario : string = '';
 
-}
+  constructor(private ClienteService: ClienteService){ }
+
+  ngOnInit(): void {
+    this.tituloFormulario = 'Nova Conta';
+    this.formulario = new FormGroup({
+      id : new FormControl(null),
+      CPF : new FormControl(null),
+      Name : new FormControl(null),
+      DataNascimento : new FormControl(null),
+      Email : new FormControl(null),
+      Telefone : new FormControl(null),
+      
+      })
+  }
+  enviarFormulario():void{
+    const Cliente: ClienteService = this.formulario.value;
+    this.ClienteService.criar(Cliente).subscribe(result=>{
+      alert("Emprestimos inserida com sucesso!");
+    })
