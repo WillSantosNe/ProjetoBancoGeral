ca0fa24c0f954a60916ebb23472bfd378cde54b9
William Dalla Stella dos Santos <williamsantosstella@gmail.com>
2023-11-24 17:30:06 -0300

    Ajuste

---
+++ src/app/components/emprestimos/emprestimos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-emprestimos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './emprestimos.component.html',
+  styleUrl: './emprestimos.component.css'
+})
+export class EmprestimosComponent {
+
+}
